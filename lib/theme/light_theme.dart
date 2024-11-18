cc591f75da3241bd457d433310c95dd81bf09ccf
Shridhar2205 <golugupta981@gmail.com>
2024-11-19 03:34:10 +0530

    small change

--- lib/theme/light_theme.dart
+++ lib/theme/light_theme.dart
@@ -3,7 +3,7 @@ import 'package:flutter/material.dart';
 ThemeData lightTheme = ThemeData(
   brightness: Brightness.light,
   appBarTheme: const AppBarTheme(
-    backgroundColor: Colors.black,
+    backgroundColor: Colors.transparent,
     elevation: 0,
     iconTheme: IconThemeData(color: Colors.black),
     titleTextStyle: TextStyle(color: Colors.black, fontSize: 20),
